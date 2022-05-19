cbb8cbb1e8ef2d97ca26e74bfa2c2953b798a897
Thanatat Tamtan <acoshift@gmail.com>
2022-05-20 00:44:32 +0700

    cleanup

--- src/routes/deployment/index.svelte
+++ src/routes/deployment/index.svelte
@@ -84,34 +84,6 @@
 					<tr>
 						<td>
 							<DeploymentStatusIcon action={it.action} status={it.status} url={it.statusUrl} />
-							{#if it.action === 'pause'}
-								<i class="fas fa-pause _cl-warning-500 _mgh-12px"></i>
-							{:else if it.status === 'success'}
-	<!--							<span x-data="{ status: null }" x-init="() => {-->
-	<!--								fetch('{{.StatusURL}}')-->
-	<!--									.then((resp) => resp.json())-->
-	<!--									.then((data) => {-->
-	<!--										status = data-->
-	<!--									})-->
-	<!--							}">-->
-	<!--								<i x-show="status === null" class="fas fa-spin fa-spinner _cl-light _mgh-12px"></i>-->
-	<!--								<template x-if="status !== null">-->
-	<!--									<span>-->
-	<!--										<i x-show="status.ready === status.count" class="fas fa-check-circle _cl-positive-500 _mgh-12px"></i>-->
-	<!--										<i x-show="status.ready !== status.count" class="fas fa-exclamation-triangle _cl-warning-500 _mgh-12px"></i>-->
-	<!--									</span>-->
-	<!--								</template>-->
-	<!--							</span>-->
-							{:else}
-
-							{/if}
-							<!--{{if eq .Action.String "pause"}}-->
-							<!--{{else}}-->
-							<!--{{if eq .Status.String "success"}}-->
-							<!--{{else}}-->
-							<!--{{template "status-icon" .Status}}-->
-							<!--{{end}}-->
-							<!--{{end}}-->
 							<a class="moon-link" href={`/deployment/metrics?project=${project}&location=${it.location}&name=${it.name}`}>
 								{it.name}
 							</a>
