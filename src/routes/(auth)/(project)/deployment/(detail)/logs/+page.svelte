5a6730bf9d5a6fff1b03a8f940ecfd850a000bdb
Kimbius <kimbius.no1@gmail.com>
2022-12-23 21:39:26 +0700

    Markup deployment/logs

--- src/routes/(auth)/(project)/deployment/(detail)/logs/+page.svelte
+++ src/routes/(auth)/(project)/deployment/(detail)/logs/+page.svelte
@@ -1,36 +1,51 @@
 <script>
-	import { onMount } from 'svelte'
+	import { onMount } from "svelte";
+	import ansi from "ansi-to-html";
 
-	export let data
-	$: ({ deployment } = data)
+	export let data;
+	$: ({ deployment } = data);
 
-	let buffer = ''
-	let text = ''
+	const ansiCoverter = new ansi();
+	let buffer = "";
+	let text = "";
 
 	onMount(() => {
-		const source = new EventSource(deployment.logUrl)
-		source.addEventListener('open', () => {
-			buffer = ''
-		})
-		source.addEventListener('message', (ev) => {
+		const source = new EventSource(deployment.logUrl);
+		source.addEventListener("open", () => {
+			buffer = "";
+		});
+		source.addEventListener("message", (ev) => {
 			try {
-				const d = JSON.parse(ev.data)
-				buffer = `${d.pod} ${d.timestamp} ${d.log}\n` + buffer
+				const d = JSON.parse(ev.data);
+				buffer = `${d.pod} ${d.log}\n${buffer}`;
 			} catch (err) {}
-		})
+		});
 		const interval = setInterval(() => {
 			if (text !== buffer) {
-				text = buffer
+				text = buffer;
 			}
-		}, 1000)
+		}, 1000);
 
 		return () => {
-			source.close()
-			clearInterval(interval)
-		}
-	})
+			source.close();
+			clearInterval(interval);
+		};
+	});
 </script>
 
 <h6><strong>Logs</strong></h6>
-<a class="button -info -small _jtfs-fst" href={`${deployment.logUrl}&type=text&raw=1`} target="_blank">Stream Raw Logs</a>
-<pre class="_pdv-30px pre-scoll" id="js-logs">{text}</pre>
+<a
+	class="button -info -small _jtfs-fst"
+	href={`${deployment.logUrl}&type=text&raw=1`}
+	target="_">Stream Raw Logs</a
+>
+<pre class="_pdv-30px pre-scoll" id="js-logs">{@html ansiCoverter.toHtml(
+		text
+	)}</pre>
+
+<style>
+	.pre-scoll {
+		overflow-x: auto;
+		font-size: 12px;
+	}
+</style>
