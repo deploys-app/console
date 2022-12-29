6efb14d7ea9c30b6f675efc362755426c2f3de3c
Thanatat Tamtan <acoshift@gmail.com>
2022-12-29 14:09:36 +0700

    refactor StatusIcon

--- src/lib/components/StatusIcon.svelte
+++ src/lib/components/StatusIcon.svelte
@@ -1,7 +1,7 @@
 <script>
 	export let status
 
-	const iconClass = {
+	const iconClassByStatus = {
 		pending: 'fa-solid fa-spinner-third fa-spin',
 		success: 'fa-solid fa-check-circle _cl-positive-500',
 		error: 'fa-solid fa-times _cl-negative-500',
@@ -9,9 +9,7 @@
 		verify: 'fa-solid fa-exclamation-triangle _cl-warning-500'
 	}
 
-	function getIconClass () {
-		return iconClass[status] || 'fa-solid fa-minus _cl-light'
-	}
+	$: iconClass = iconClassByStatus[status] || 'fa-solid fa-minus _cl-light'
 </script>
 
-<i class={`${getIconClass()} _mgh-12px`}></i>
+<i class={`${iconClass} _mgh-12px`}></i>
