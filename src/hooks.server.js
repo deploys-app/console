6cf9fc605aaa57c3acfa6d10416107eb5f4e5bc6
Thanatat Tamtan <acoshift@gmail.com>
2023-06-03 16:27:49 +0700

    disable sentry server side

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,12 +1,12 @@
-import * as Sentry from '@sentry/sveltekit'
+// import * as Sentry from '@sentry/sveltekit'
 import { sequence } from '@sveltejs/kit/hooks'
-import { env } from '$env/dynamic/public'
+// import { env } from '$env/dynamic/public'
 
-if (env.PUBLIC_SENTRY_DSN) {
-	Sentry.init({
-		dsn: env.PUBLIC_SENTRY_DSN
-	})
-}
+// if (env.PUBLIC_SENTRY_DSN) {
+//  Sentry.init({
+//     dsn: env.PUBLIC_SENTRY_DSN
+//  })
+// }
 
 const allowTheme = {
 	dark: true,
@@ -51,9 +51,9 @@ function storeProject ({ event, resolve }) {
 }
 
 export const handle = sequence(
-	Sentry.sentryHandle(),
+	// Sentry.sentryHandle(),
 	theme,
 	handleCookie,
 	storeProject
 )
-export const handleError = Sentry.handleErrorWithSentry()
+// export const handleError = Sentry.handleErrorWithSentry()
