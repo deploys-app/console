ab6f7d05a584427970a627724ae22a1452d376f7
Thanatat Tamtan <acoshift@gmail.com>
2023-11-21 18:25:38 +0700

    implement dropbox

---
+++ src/routes/(auth)/(project)/dropbox/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+	export let data
+
+	$: project = data.project
+
+	/** @type {HTMLInputElement} */
+	let elFile
+
+	let downloadUrl = ''
+	let uploading = false
+
+	async function upload () {
+		if (uploading) {
+			return
+		}
+		if (!elFile.files?.length) {
+			return
+		}
+
+		const file = elFile.files[0]
+
+		downloadUrl = ''
+		uploading = true
+		try {
+			const resp = await fetch('#', {
+				method: 'POST',
+				body: file
+			})
+			const res = await resp.json()
+			downloadUrl = res.downloadUrl ?? ''
+			elFile.value = ''
+		} catch (e) {
+			console.log(e)
+		} finally {
+			uploading = false
+		}
+	}
+</script>
+
+<h6>Dropbox (Alpha)</h6>
+<br>
+<div class="nm-panel is-level-300">
+	<form class="_dp-g _g-6 _w-100pct" on:submit|preventDefault={upload}>
+		<div class="nm-field">
+			<input class="nm-field" type="file" name="file" bind:this={elFile}>
+		</div>
+		<div class="_dp-f _g-6">
+			<button class="nm-button" class:is-loading={uploading} type="submit">Upload</button>
+		</div>
+	</form>
+
+	{#if downloadUrl}
+		<div class="_mgt-8">
+			Download URL
+			<pre>{downloadUrl}</pre>
+		</div>
+	{/if}
+</div>
---
+++ src/routes/(auth)/(project)/dropbox/+server.js
@@ -0,0 +1,18 @@
+/** @type {import('./$types').RequestHandler} */
+export async function POST ({ request }) {
+	const resp = await fetch('https://dropbox.deploys.app/', {
+		method: 'POST',
+		body: request.body,
+		duplex: 'half',
+		headers: {
+			accept: 'application/json',
+			'content-type': request.headers.get('content-type') ?? 'application/octet-stream'
+		}
+	})
+	return new Response(resp.body, {
+		status: resp.status,
+		headers: {
+			'content-type': resp.headers.get('content-type') ?? ''
+		}
+	})
+}
