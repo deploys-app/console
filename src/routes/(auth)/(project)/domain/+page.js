e3459a92a08d07f56faddf14f4804f59d41ec3ab
Thanatat Tamtan <acoshift@gmail.com>
2023-12-16 08:00:10 +0700

    stream domain list data

--- src/routes/(auth)/(project)/domain/+page.js
+++ src/routes/(auth)/(project)/domain/+page.js
@@ -1,18 +1,8 @@
-import { error } from '@sveltejs/kit'
 import api from '$lib/api'
 
 export async function load ({ parent, fetch }) {
 	const { project } = await parent()
-
-	const domains = await api.invoke('domain.list', { project }, fetch)
-	if (!domains.ok && !domains.error?.forbidden) {
-		error(500, domains.error?.message)
-	}
-
 	return {
-		permission: {
-			domains: !domains.error?.forbidden
-		},
-		domains: domains.result?.items ?? []
+		domains: api.invoke('domain.list', { project }, fetch)
 	}
 }
